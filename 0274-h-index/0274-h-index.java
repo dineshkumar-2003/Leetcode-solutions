68bfaf28095fdd3674c920927fdf8c99bd15e5f1
Dineshkumar  S <103087157+dineshkumar-2003@users.noreply.github.com>
2024-12-26 20:14:49 +0530

    Time: 0 ms (100%), Space: 41.4 MB (80.26%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        int[] freq = new int[n+1];
+        for(int i=0;i<n;i++){
+            if(citations[i]>=n)
+                freq[n]+=1;
+            else
+                freq[citations[i]]+=1;
+            
+        }   
+        int idx=n;
+        int s=freq[idx];
+        while(s<idx){
+            idx--;
+            s+=freq[idx];
+        }     
+        return idx;
+    }
+}
